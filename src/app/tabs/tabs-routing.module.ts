71f9c045947dc089005d78dd03239a93c88a1285
shammerschmied <hammerschmiedstefan@gmail.com>
2022-04-22 11:30:10 +0200

    Loginseite erstellt

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -4,7 +4,7 @@ import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
   {
-    path: 'tabs',
+    path: 'Eventivity',
     component: TabsPage,
     children: [
       {
@@ -20,15 +20,19 @@ const routes: Routes = [
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
       {
+        path: 'Login',
+        loadChildren: () => import('../Login/login.module').then(m => m.LoginPageModule)
+      },
+      {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/Login/login',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/Login/login.page.html',
     pathMatch: 'full'
   }
 ];
